d1826ea9756486a7a48e4c3dfa35328703645b44
DynamicApproach <89496209+DynamicApproach@users.noreply.github.com>
2022-02-15 12:37:48 -0500

    Add files via upload

---
+++ InputMethodDemo2/InputMethodDemo2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace InputMethodDemo2
+{
+    using static System.Console;
+    using System;
+    class InputMethodDemo2
+    {
+       static int first, second;
+        static void Main()
+        {
+            InputMethod(out first, out second);
+            WriteLine("After InputMethod first is {0}", first); 
+            WriteLine("and second is {0}", second);
+        } 
+        private static void InputMethod(out int one, out int two)
+        {
+            string s1, s2;
+            Write("Enter first integer ");
+            s1 = ReadLine();
+            Write("Enter second integer ");
+            s2 = ReadLine();
+            one = Convert.ToInt32(s1);
+            two = Convert.ToInt32(s2);
+        }
+    
+        public static int DataEntry(string whichOne)
+        {
+            Write("Enter integer {0}", whichOne);
+            return  Convert.ToInt32(ReadLine());
+        }
+
+    }
+}
